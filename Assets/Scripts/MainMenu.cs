caef03c1553b78dd1b5cf99978e9e590c3a82c6b
Wessel <467551@student.saxion.nl>
2021-06-03 13:03:10 +0200

    Added simple first menu code
    Added simple first menu code

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using Bolt;
+using Bolt.Matchmaking;
+using UdpKit;
+
+public class MainMenu : GlobalEventListener
+{
+    private string hostSessionID = "default", joinSessionID = "default";
+ 
+    // ###### Server hosting ######
+    public void StartServer(string pSessionID) {
+        hostSessionID = pSessionID;
+        BoltLauncher.StartServer();
+    }
+
+    public override void BoltStartDone() {
+        BoltMatchmaking.CreateSession(hostSessionID,sceneToLoad:"Game");
+    }
+
+    // ###### Server Joining ######
+    public void StartClient(string pSessionID) {
+        joinSessionID = pSessionID;
+        BoltLauncher.StartClient();
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        foreach(var session in sessionList){
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon) {
+                BoltMatchmaking.JoinSession(photonSession);
+            }
+        }
+    }
+}
